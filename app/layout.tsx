7daa7b11e1e0beb167d0d31ada12409893321a37
cheeter_Lee <johnson991210@gmail.com>
2023-01-06 12:03:45 +0800

    header setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "../styles/globals.css"
+import Header from "./Header"
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html>
+			<head />
+			<body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+        <Header />
+				<div className="max-w-6xl mx-auto">
+          {children}
+        </div>
+			</body>
+		</html>
+	)
+}
