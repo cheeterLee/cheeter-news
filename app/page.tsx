e19ead5f3d0886a6c1797bbeaac9a64ec34672ca
cheeter_Lee <johnson991210@gmail.com>
2023-01-06 15:46:07 +0800

    create mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { categories } from "../constants"
 import fetchNews from "../lib/fetchNews"
+import NewsList from "./NewsList"
+// Mock data
+import response from '../response.json'
 
 
 async function Homepage() {
     // fetch the news data
-    const news: NewsResponse = await fetchNews(categories.join(','))
-    console.log("🚀 ~ file: page.tsx:8 ~ Homepage ~ news", news)
-    
+    // const news: NewsResponse = await fetchNews(categories.join(','))
+
+    // using mock data
+    const news: any = response 
 
 	return <div>
-        {/* newslist */}
+       <NewsList news={news} />
     </div>
 }
 
