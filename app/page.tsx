df426b40961c77635d42fa79f9270d1336cc8e9e
cheeter_Lee <johnson991210@gmail.com>
2023-01-06 17:58:03 +0800

    remove mock data

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import { categories } from "../constants"
 import fetchNews from "../lib/fetchNews"
 import NewsList from "./NewsList"
 // Mock data
-import response from '../response.json'
+// import response from '../response.json'
 
 
 async function Homepage() {
     // fetch the news data
-    // const news: NewsResponse = await fetchNews(categories.join(','))
+    const news: NewsResponse = await fetchNews(categories.join(','))
 
     // using mock data
-    const news: any = response 
+    // const news: any = response 
 
 	return <div>
        <NewsList news={news} />
